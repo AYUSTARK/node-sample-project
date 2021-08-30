078ee357b98c7346ac1ca1ca5d8471d71599581e
AYUSTARK <ayushrj65@gmail.com>
2021-08-30 18:21:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const socketio = require('socket.io')
+const {Server} = require('socket.io')
 const http = require('http')
 const cors = require('cors')
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = new Server(server)
 // Parses the data as json
 app.use(cors())
 app.use(express.json())
